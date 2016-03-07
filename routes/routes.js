6bbfb51904ce02047ccd1f0eaa998fa613154b71
Aleah Ellis <aleah.is@icloud.com>
2016-03-07 09:08:47 -0500

    delete node_modules, add .gitignore

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,18 @@
-app.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'homeview.html',
-      controller: 'homeContr'
-    })
-    .when('/external', {
-      templateUrl: 'externalSrcView.html',
-      controller: 'externalSrcContr',
-    })
-    .when('/kata', {
-      templateUrl: 'chapter.html',
-      controller: 'ChapterController'
-    })
-    .otherwise({ 
-      redirectTo: '/'
-    });
-});
+// app.config(function($routeProvider) {
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'homeview.html',
+//       controller: 'homeContr'
+//     })
+//     .when('/external', {
+//       templateUrl: 'externalSrcView.html',
+//       controller: 'externalSrcContr',
+//     })
+//     .when('/kata', {
+//       templateUrl: 'chapter.html',
+//       controller: 'ChapterController'
+//     })
+//     .otherwise({ 
+//       redirectTo: '/'
+//     });
+// });
