43c783071058d17d8a362ff85c7111b8b987164e
Aleah Ellis <aleah.is@icloud.com>
2016-03-07 14:41:40 -0500

    added stuff to controller, still non functional

--- controllers/externalSrcContr.js
+++ controllers/externalSrcContr.js
@@ -1,5 +1,15 @@
 angular.module('mod')
-	.controller("theSource",function($scope){
-  		
-  		});
-  		 console.log("see a thing?")
+	.controller("theSource",function($scope, $http){
+			 method: 'GET',
+ 			 url: '//www.pinterest.com/malloryontravel/cool-images/json'
+			}).then(function successCallback(response) {
+		    	//how do we want to display?
+		  	
+		  	}, function errorCallback(response) {
+		     //404 error
+	});
+
+		  	
+		  	console.log("see a thing?")
+
+		  		
