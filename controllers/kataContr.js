d156b53874e2c26f24e4ec4e5fa87e3b0305185f
Aleah Ellis <aleah.is@icloud.com>
2016-03-07 16:49:04 -0500

    working on kata cont,added kata service file

--- controllers/kataContr.js
+++ controllers/kataContr.js
@@ -1,12 +1,19 @@
 angular.module('mod')
-	.controller("kataConr",function($scope){
+	.controller("kataConr",function($scope, ){
 		
-		function palindrome(str) {
-		    str = str.toLowerCase();
-		    var reverseStr = str.split("").reverse().join('');
-		    return str === reverseStr;
-		}
-		return palindrome.str //? 
+		var str="eye";
+			function palindrome(str) {
+			    str = str.toLowerCase();
+			    var reverseStr = str.split("").reverse().join('');
+			    if (str === reverseStr){
+			    return  "yes congratulations, you found a  Palindrome";
+			    }else{
+			    return "aww man, that is not a palidrome";
+			    }
+			}
+
+			console.log(palindrome(str));
+
   		
   		});
   		 console.log("Kata, Baby!")
