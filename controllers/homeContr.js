855b2f699bca5a14ef39153f0f45c7f911c4d9b3
KJ <kj.schmidt913@gmail.com>
2016-03-08 17:52:37 -0500

    customized directives to Oscar and Woof. Added scope to the home controller. Little styling things

--- controllers/homeContr.js
+++ controllers/homeContr.js
@@ -1,15 +1,18 @@
-app.controller("homeContr", function(){
-  console.log("welcome home")
-});
+app.controller("homeContr", ['$scope', function($scope){
+  $scope.homeImage = {
+  	img: "images/oscarTongue.jpg",
+  	caption: "Woof."
+  }
+}]);
 
 app.directive("firstDirective", function(){
 	return{
-		template: "<h1 id='directiveOne'>First Directive</h1>"
+		template: "<img src='{{homeImage.img}}' /img>"
 	};
 });
 
 app.directive("secondDirective", function(){
 	return{
-		template: "<h3 id='directiveTwo'>Second Directive</h3>"
+		template: "<h3 id='directiveTwo'>{{homeImage.caption}}</h3>"
 	};
 });
